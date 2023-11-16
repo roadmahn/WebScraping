ef16bbbe90185b1bb2ddd3dc8d2687a2726ef436
roadmahn <olayinkaoluyede@gmail.com>
2023-11-16 15:54:09 +0100

    move

--- web scraping.py
+++ web scraping.py
@@ -41,28 +41,28 @@ title_name =  [item.text for item in title]
 df = pd.DataFrame(columns = title_name)
 df
 # scrolling through the dfferent pages
-pages = soup.find('li' , class_="waves-effect")
-
-for page in pages:
+#pages = soup.find('li' , class_="waves-effect")
+#
+#for page in pages:
 
 #print(datafrm)
 # finding each row of data in the table, the rows are tagged with a 'tr'
-    column_data = tab.find_all('tr')
-    #we now loop through each row to find individual data which is tagged by 'td'
-    for row in column_data[1:]:
-        row_data = row.find_all('td')
-    #    then loop through each individual dtat to clean out the information
-        data = [info.text.strip() for info in row_data]
-    #trying to eliminate rows without any data
-        if len(data) == 1:
-            continue
-    #    print(data)
-    #    print(len(data))
-    #    add each row to the dataframe by checking the length of the dataframe.
-        length = len(df)
-        df.loc[length] = data
-    #    print(df)
-    #    datafrm = pd.DataFrame(rows = row)
-    print(df)
+column_data = tab.find_all('tr')
+#we now loop through each row to find individual data which is tagged by 'td'
+for row in column_data[1:]:
+    row_data = row.find_all('td')
+#    then loop through each individual dtat to clean out the information
+    data = [info.text.strip() for info in row_data]
+#trying to eliminate rows without any data
+    if len(data) == 1:
+        continue
+#    print(data)
+#    print(len(data))
+#    add each row to the dataframe by checking the length of the dataframe.
+    length = len(df)
+    df.loc[length] = data
+#    print(df)
+#    datafrm = pd.DataFrame(rows = row)
+print(df)
 # pushing the dataframe into a csv file and removing the extra column with(index = false)
 df.to_csv(r'D:\Scripts\scrape.csv', index = False)
